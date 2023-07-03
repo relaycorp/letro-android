45c509de35c92ac148e81c709fa3d0168bb567d9
Gabriel Gircenko <gabriel.gircenko@gmail.com>
2023-07-03 10:52:06 +0200

    moved navigation to a separate composable

--- app/src/main/java/tech/realycorp/letro/ui/main/MainActivity.kt
+++ app/src/main/java/tech/realycorp/letro/ui/main/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -17,7 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -75,55 +74,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    NavHost(
+                    NavHostContainer(
                         navController = navController,
-                        startDestination = Route.Splash.name,
-                    ) {
-                        composable(Route.AccountConfirmation.name) {
-                            systemUiController.isStatusBarVisible = true
-                            ActionTakingScreen(ActionTakingScreenUIStateModel.AccountConfirmation)
-                        }
-                        composable(Route.AccountCreation.name) {
-                            systemUiController.isStatusBarVisible = false
-                            AccountCreationScreen(
-                                onCreateAccount = {
-                                    navController.navigate(
-                                        Route.WaitingForAccountCreation.name,
-                                    ) // TODO Change to correct functionality
-                                },
-                                onUseExistingAccount = {
-                                    navController.navigate(
-                                        Route.AccountConfirmation.name,
-                                    ) // TODO Change to correct functionality
-                                },
+                        showStatusBar = { show ->
+                            systemUiController.isStatusBarVisible = show
+                        },
+                        onNavigateToGooglePlay = {
+                            startActivity(
+                                Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse("https://play.google.com/store/apps/details?id=tech.relaycorp.gateway"),
+                                ),
                             )
-                        }
-                        composable(Route.GatewayNotInstalled.name) {
-                            systemUiController.isStatusBarVisible = false
-                            GatewayNotInstalledScreen(
-                                onNavigateToGooglePlay = {
-                                    startActivity(
-                                        Intent(
-                                            Intent.ACTION_VIEW,
-                                            Uri.parse("https://play.google.com/store/apps/details?id=tech.relaycorp.gateway"),
-                                        ),
-                                    )
-                                },
-                            )
-                        }
-                        composable(Route.PairingRequestSent.name) {
-                            systemUiController.isStatusBarVisible = true
-                            ActionTakingScreen(ActionTakingScreenUIStateModel.PairingRequestSent)
-                        }
-                        composable(Route.Splash.name) {
-                            systemUiController.isStatusBarVisible = false
-                            SplashScreen()
-                        }
-                        composable(Route.WaitingForAccountCreation.name) {
-                            systemUiController.isStatusBarVisible = true
-                            ActionTakingScreen(ActionTakingScreenUIStateModel.Waiting)
-                        }
-                    }
+                        },
+                    )
                 }
             }
         }
@@ -131,17 +95,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LetroTheme {
-        Greeting("Android")
+fun NavHostContainer(
+    navController: NavHostController,
+    showStatusBar: (Boolean) -> Unit,
+    onNavigateToGooglePlay: () -> Unit,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Route.Splash.name,
+    ) {
+        composable(Route.AccountConfirmation.name) {
+            showStatusBar(true)
+            ActionTakingScreen(ActionTakingScreenUIStateModel.AccountConfirmation)
+        }
+        composable(Route.AccountCreation.name) {
+            showStatusBar(false)
+            AccountCreationScreen(
+                onCreateAccount = {
+                    navController.navigate(
+                        Route.WaitingForAccountCreation.name,
+                    ) // TODO Change to correct functionality
+                },
+                onUseExistingAccount = {
+                    navController.navigate(
+                        Route.AccountConfirmation.name,
+                    ) // TODO Change to correct functionality
+                },
+            )
+        }
+        composable(Route.GatewayNotInstalled.name) {
+            showStatusBar(false)
+            GatewayNotInstalledScreen(
+                onNavigateToGooglePlay = onNavigateToGooglePlay,
+            )
+        }
+        composable(Route.PairingRequestSent.name) {
+            showStatusBar(true)
+            ActionTakingScreen(ActionTakingScreenUIStateModel.PairingRequestSent)
+        }
+        composable(Route.Splash.name) {
+            showStatusBar(false)
+            SplashScreen()
+        }
+        composable(Route.WaitingForAccountCreation.name) {
+            showStatusBar(true)
+            ActionTakingScreen(ActionTakingScreenUIStateModel.Waiting)
+        }
     }
 }
