286f8080886c50b6626019517552917ce275333c
Gabriel Gircenko <gabriel.gircenko@gmail.com>
2023-07-13 10:29:52 +0200

    fixed status bar color in dark mode

--- app/src/main/java/tech/relaycorp/letro/ui/theme/Theme.kt
+++ app/src/main/java/tech/relaycorp/letro/ui/theme/Theme.kt
@@ -3,17 +3,26 @@ package tech.relaycorp.letro.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
@@ -24,12 +33,13 @@ private val DarkColorScheme = darkColorScheme(
 //    onSecondary = Color.White,
 //    tertiary = SecondaryPink,
 //    onTertiary = Color.White,
-    surface = Grey100,
+//    surface = Grey100,
+    surface = Error1,
     onSurface = Grey20,
     surfaceVariant = Grey80,
     onSurfaceVariant = Grey40,
     primaryContainer = Grey80,
-    onPrimaryContainer = Primary20,
+    onPrimaryContainer = Primary20, // Currently used for the "disabled" state
     secondaryContainer = Grey60,
     background = Grey100,
     onBackground = Grey20,
@@ -93,3 +103,49 @@ fun LetroTheme(
         content = content,
     )
 }
+
+
+@Composable
+private fun LetroThemeContent() {
+    Column {
+        Button(onClick = { /*TODO*/ }) {
+            Text("Button")
+        }
+        Spacer(modifier = Modifier.size(ItemPadding))
+
+        TextField(value = "", onValueChange = {})
+        Spacer(modifier = Modifier.size(ItemPadding))
+
+        TextField(value = "text field", onValueChange = {})
+        Spacer(modifier = Modifier.size(ItemPadding))
+
+        TextField(value = "error", onValueChange = {}, isError = true)
+        Spacer(modifier = Modifier.size(ItemPadding))
+
+        OutlinedTextField(value = "", onValueChange = {})
+        Spacer(modifier = Modifier.size(ItemPadding))
+
+        OutlinedTextField(value = "outlined text field", onValueChange = {})
+        Spacer(modifier = Modifier.size(ItemPadding))
+
+        OutlinedTextField(value = "error", onValueChange = {}, isError = true)
+        Spacer(modifier = Modifier.size(ItemPadding))
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun LetroThemeLightPreview() {
+    LetroTheme(darkTheme = false) {
+        LetroThemeContent()
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun LetroThemeDarkPreview() {
+    LetroTheme(darkTheme = true) {
+        LetroThemeContent()
+    }
+}
+
