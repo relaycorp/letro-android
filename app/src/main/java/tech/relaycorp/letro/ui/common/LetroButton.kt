c60d9d5fbcd2d24edcf9b283e41c542a01d0dc55
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2023-09-21 14:53:29 +0200

    fix: fix fonts (#60)
    Fix fonts

--- app/src/main/java/tech/relaycorp/letro/ui/common/LetroButton.kt
+++ app/src/main/java/tech/relaycorp/letro/ui/common/LetroButton.kt
@@ -15,7 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import tech.relaycorp.letro.ui.theme.LargeProminent
+import tech.relaycorp.letro.ui.theme.LabelLargeProminent
 import tech.relaycorp.letro.ui.theme.LetroColor
 
 @Composable
@@ -69,7 +69,7 @@ fun LetroButton(
         }
         Text(
             text = text,
-            style = MaterialTheme.typography.LargeProminent,
+            style = MaterialTheme.typography.LabelLargeProminent,
         )
     }
 }
