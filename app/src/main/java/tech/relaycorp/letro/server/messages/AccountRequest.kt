cd089cdf920a697c0ae65761225447516f83d857
Gus Narea <gnarea@users.noreply.github.com>
2023-09-25 14:22:01 +0100

    AccountRequest: Remove duplicated code

--- app/src/main/java/tech/relaycorp/letro/server/messages/AccountRequest.kt
+++ app/src/main/java/tech/relaycorp/letro/server/messages/AccountRequest.kt
@@ -6,6 +6,7 @@ import org.bouncycastle.asn1.DERVisibleString
 import tech.relaycorp.letro.utils.asn1.ASN1Utils
 import tech.relaycorp.letro.utils.crypto.RSASigning
 import tech.relaycorp.letro.utils.crypto.spkiEncode
+import tech.relaycorp.letro.utils.i18n.normaliseString
 import java.security.PrivateKey
 import java.security.PublicKey
 import java.util.Locale
@@ -33,16 +34,5 @@ class AccountRequest(
         return ASN1Utils.serializeSequence(listOf(requestEncoded, DERBitString(signature)), false)
     }
 
-    private fun encodeLocale(): DERVisibleString {
-        val languageCode = locale.language.lowercase()
-        val countryCode = locale.country.lowercase()
-        val localeNormalised = if (languageCode.isEmpty()) {
-            ""
-        } else if (countryCode.isEmpty()) {
-            languageCode
-        } else {
-            "$languageCode-$countryCode"
-        }
-        return DERVisibleString(localeNormalised)
-    }
+    private fun encodeLocale() = DERVisibleString(locale.normaliseString())
 }
