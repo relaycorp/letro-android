241a2d4afd765e85741eebefda360b5384fe7cbd
Mikhail Guliaev <48011967+mgulyaev10@users.noreply.github.com>
2023-10-24 00:42:03 +0200

    fix: fix glitches on awala initialization screens + fix horizontal orienta… (#145)

--- app/src/main/java/tech/relaycorp/letro/account/registration/ui/RegistrationScreen.kt
+++ app/src/main/java/tech/relaycorp/letro/account/registration/ui/RegistrationScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -58,6 +60,7 @@ fun RegistrationScreen(
     ) {
         Column(
             modifier = Modifier
+                .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
                 .padding(
                     horizontal = HorizontalScreenPadding,
