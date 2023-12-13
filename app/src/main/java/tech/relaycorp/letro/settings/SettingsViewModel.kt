97752bbb9b834347d64d341c8fad0cc864d131eb
migulyaev <mgulyaev2702@gmail.com>
2023-12-13 13:32:31 +0100

    LTAND-171: forbid creating more than 2 accounts

--- app/src/main/java/tech/relaycorp/letro/settings/SettingsViewModel.kt
+++ app/src/main/java/tech/relaycorp/letro/settings/SettingsViewModel.kt
@@ -1,18 +1,20 @@
 package tech.relaycorp.letro.settings
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import tech.relaycorp.letro.account.model.Account
+import tech.relaycorp.letro.account.model.AccountType
 import tech.relaycorp.letro.account.storage.repository.AccountRepository
 import tech.relaycorp.letro.account.utils.AccountsSorter
+import tech.relaycorp.letro.base.BaseViewModel
+import tech.relaycorp.letro.settings.SettingsViewModel.Companion.MAX_FREE_ACCOUNTS
+import tech.relaycorp.letro.utils.coroutines.Dispatchers
 import tech.relaycorp.letro.utils.di.AppVersion
 import javax.inject.Inject
 
@@ -21,16 +23,32 @@ class SettingsViewModel @Inject constructor(
     private val accountRepository: AccountRepository,
     private val accountsSorter: AccountsSorter,
     @AppVersion val appVersion: String,
-) : ViewModel() {
+    dispatchers: Dispatchers,
+) : BaseViewModel(dispatchers) {
 
-    val accounts: StateFlow<List<Account>>
-        get() = accountRepository.allAccounts
-            .map(accountsSorter::withCurrentAccountFirst)
-            .stateIn(viewModelScope, SharingStarted.Eagerly, accountRepository.allAccounts.value)
+    private val _uiState = MutableStateFlow(SettingsUiState())
+    val uiState: StateFlow<SettingsUiState>
+        get() = _uiState
 
     private val _deleteAccountConfirmationDialog = MutableStateFlow(DeleteAccountDialogState())
     val deleteAccountConfirmationDialog: StateFlow<DeleteAccountDialogState>
         get() = _deleteAccountConfirmationDialog
+
+    init {
+        viewModelScope.launch(dispatchers.IO) {
+            accountRepository.allAccounts
+                .map(accountsSorter::withCurrentAccountFirst)
+                .collect { accounts ->
+                    val createdAccounts = accounts.count { it.accountType == AccountType.CREATED_FROM_SCRATCH }
+                    _uiState.update {
+                        it.copy(
+                            accounts = accounts,
+                            infoViewType = if (createdAccounts >= MAX_FREE_ACCOUNTS) SettingsAccountsInfoViewType.Warning(MAX_FREE_ACCOUNTS) else SettingsAccountsInfoViewType.Info(createdAccounts = createdAccounts, maxFreeAccounts = MAX_FREE_ACCOUNTS),
+                        )
+                    }
+                }
+        }
+    }
 
     fun onAccountDeleteClick(account: Account) {
         _deleteAccountConfirmationDialog.update {
@@ -56,9 +74,32 @@ class SettingsViewModel @Inject constructor(
             )
         }
     }
+
+    companion object {
+        const val MAX_FREE_ACCOUNTS = 2
+    }
 }
+
+data class SettingsUiState(
+    val accounts: List<Account> = emptyList(),
+    val infoViewType: SettingsAccountsInfoViewType = SettingsAccountsInfoViewType.Info(
+        createdAccounts = 0,
+        maxFreeAccounts = MAX_FREE_ACCOUNTS,
+    ),
+)
 
 data class DeleteAccountDialogState(
     val isShown: Boolean = false,
     val account: Account? = null,
 )
+
+sealed interface SettingsAccountsInfoViewType {
+    data class Info(
+        val createdAccounts: Int,
+        val maxFreeAccounts: Int,
+    ) : SettingsAccountsInfoViewType
+
+    data class Warning(
+        val maxFreeAccounts: Int,
+    ) : SettingsAccountsInfoViewType
+}
--- app/src/main/java/tech/relaycorp/letro/ui/common/LetroInfoView.kt
+++ app/src/main/java/tech/relaycorp/letro/ui/common/LetroInfoView.kt
@@ -12,19 +12,24 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import tech.relaycorp.letro.R
 
 @Composable
 fun LetroInfoView(
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier,
+    backgroundColor: Color = MaterialTheme.colorScheme.surfaceVariant,
+    infoIconColor: Color = MaterialTheme.colorScheme.onPrimaryContainer,
     content: @Composable () -> Unit,
 ) {
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .background(
-                color = MaterialTheme.colorScheme.surfaceVariant,
+                color = backgroundColor,
                 shape = RoundedCornerShape(8.dp),
             )
             .padding(8.dp),
@@ -32,8 +37,9 @@ fun LetroInfoView(
     ) {
         Icon(
             painter = painterResource(id = R.drawable.info),
-            tint = MaterialTheme.colorScheme.onPrimaryContainer,
+            tint = infoIconColor,
             contentDescription = null,
+            modifier = iconModifier,
         )
         Spacer(
             modifier = Modifier.width(8.dp),
